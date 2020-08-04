393521a384c5ea79e35f3d76ca947b3ef47f3a1e
adamoudad <adam.oudad@gmail.com>
2020-08-04 15:35:32 +0900

    Add configuration variables
    - Languages
    - Base url
    - Language codes

---
+++ deepl/constants.py
@@ -0,0 +1,15 @@
+LANGUAGES = {
+    "en": "english",
+    "fr": "french",
+    "de": "german",
+    "ja": "japanese",
+    "es": "spanish",
+    "pt": "portuguese",
+    "it": "italian",
+    "nl": "dutch",
+    "pl": "polish",
+    "ru": "russian",
+    "zh": "chinese (simplified)"
+}
+
+LANGCODES = dict(map(reversed, LANGUAGES.items()))
---
+++ deepl/urls.py
@@ -0,0 +1,1 @@
+BASE_URL = 'https://www.deepl.com/translator#{fr_lang}/{to_lang}/_'
